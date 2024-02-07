d4e4f45905c4df4013100e3b015856f0f4dda169
herzliyaaa <herzliabarangan@gmail.com>
2024-02-07 19:19:47 +0800

    Change the deprecated functions into DataSource in TypeORM

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ import * as express from "express";
 import { Express, Request, Response } from "express";
 import helmet from "helmet";
 import * as morgan from "morgan";
-import { User } from "./entities/user.entity";
-import { createConnection, getConnection } from "typeorm";
+import connectDB from "./data-source"
 import "reflect-metadata";
 
 const app: Express = express();
@@ -14,25 +13,13 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
+connectDB
+
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-
-createConnection()
-  .then(async () => {
-    console.log("Connected to database");
-  })
-  .catch((error) => {
-    console.error("Error connecting to database:", error);
-  });
-
-app.get("/users", async (req: Request, res: Response) => {
-  const userRepository = getConnection().getRepository(User);
-  const users = await userRepository.find();
-  res.json(users);
-});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at port ${port}`);
