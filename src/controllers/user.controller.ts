d2920fb89fd16a335d53f913be526bb66b96216f
herzliyaaa <herzliabarangan@gmail.com>
2024-02-14 17:27:40 +0800

    feat: Convert controllers into arrow functions
    The functions in controller were changed into arrow functions to remove
    the manually binding of this in constructor.

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -6,61 +6,53 @@ export class UserController {
 
   constructor(userService: UserService) {
     this.userService = userService;
-    this.getAllUser = this.getAllUser.bind(this);
-    this.getUserById = this.getUserById.bind(this);
-    this.createUser = this.createUser.bind(this);
-    this.updateUser = this.updateUser.bind(this);
-    this.removeUser = this.removeUser.bind(this);
   }
 
-  async getAllUser(req: Request, res: Response) {
+  getAllUser = async (req: Request, res: Response) => {
     try {
       const users = await this.userService.getAllUsers();
-      console.log("🚀 ~ UserController ~ getAllUser ~ users:", users);
+
 
       res.status(200).send(users);
     } catch (error) {
       res.status(400).send(`Error saving timesheet data: ${error.message}`);
     }
-  }
+  };
 
-  async getUserById(req: Request, res: Response) {
+  getUserById = async (req: Request, res: Response) => {
     try {
       const user = await this.userService.getUserById(req.params.id);
-      console.log("🚀 ~ UserController ~ getUserById ~ user:", user);
       res.status(200).send(user);
     } catch (error) {
       res.status(400).send(error.message);
     }
-  }
+  };
 
-  async createUser(req: Request, res: Response) {
+  createUser = async (req: Request, res: Response) => {
     try {
       const user = await this.userService.addUser(req.body);
-      console.log("🚀 ~ UserController ~ getUserById ~ user:", user);
       res.status(200).send(user);
     } catch (error) {
       res.status(400).send(error.message);
     }
-  }
+  };
 
-  async updateUser(req: Request, res: Response) {
+  updateUser = async (req: Request, res: Response) => {
     try {
       const user = await this.userService.updateUser(req.params.id, req.body);
-      console.log("🚀 ~ UserController ~ getUserById ~ user:", user);
       res.status(200).send(user);
     } catch (error) {
       res.status(400).send(error.message);
     }
-  }
+  };
 
-  async removeUser(req: Request, res: Response) {
+  removeUser = async (req: Request, res: Response) => {
     try {
       const user = await this.userService.removeUser(req.params.id);
-      console.log("🚀 ~ UserController ~ getUserById ~ user:", user);
+
       res.status(200).send(user);
     } catch (error) {
       res.status(400).send(error.message);
     }
-  }
+  };
 }
--- todo.md
+++ todo.md
@@ -2,18 +2,5 @@
 [/] Change deprecated functions in TypeORM and change it into DataSource
 https://levelup.gitconnected.com/datasource-in-typeorm-a-new-way-to-connect-to-your-database-cdc6622f9bbc
 
-[] Create Entities for this schema:
-
-    *User Table:
-
-    user_id (Primary Key), username, email, password_hash
-
-    *Expense Category Table: 
-    
-    category_id (Primary Key), category_name
-
-    *Expense Table:
-
-    expense_id (Primary Key), user_id (Foreign Key referencing User Table), 
-    category_id (Foreign Key referencing Expense Category Table), amount
-    description, date, location
+[/] Create Entities for this schema:
+[/] Convert the controller functions into arrow functions so it would not neet to be bind in constructor.
